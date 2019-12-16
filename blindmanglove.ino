737836b9a044f70f654abd8368a91f3cabd4d082
yashv11 <54174592+yashv11@users.noreply.github.com>
2019-12-16 11:35:20 +0530

    Add files via upload

---
+++ blindmanglove.ino
@@ -0,0 +1,43 @@
+#define Trig 11
+#define pin 10
+#define motor 7
+#define buzz 8
+
+
+void setup()
+{ 
+pinMode(Trig, OUTPUT);
+pinMode(pin, INPUT);
+pinMode(motor, OUTPUT);
+pinMode(buzz,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop()
+{ 
+long dist, timetaken;
+digitalWrite(Trig, LOW); 
+delayMicroseconds(20); 
+digitalWrite(Trig, HIGH);
+delayMicroseconds(10); 
+digitalWrite(Trig, LOW);
+
+timetaken = pulseIn(pin, HIGH);
+dist = ((timetaken/2)*340)/10000;
+
+dist=analogRead(A0);  
+Serial.println(dist);
+
+
+if(dist<100)
+{
+digitalWrite(motor,HIGH);
+digitalWrite(buzz,HIGH); 
+}
+else
+{
+digitalWrite(motor,LOW);
+digitalWrite(buzz,LOW); 
+} 
+delay(500);
+}
